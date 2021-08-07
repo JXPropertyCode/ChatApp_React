2c7f7509595bf7aba4aab0cdc2f19870f580e32e
JXPropertyCode <yellowdice92@hotmail.com>
2021-08-07 16:53:13 -0700

    added input box and send button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./components/messageBox.css";
 
 var W3CWebSocket = require("websocket").w3cwebsocket;
@@ -33,14 +33,55 @@ client.onmessage = function (e) {
 };
 
 function App() {
+	const [message, setMessage] = useState("");
+
+	const messageInput = (e) => {
+		// e.target.value gets the entire input bar's values
+		e.preventDefault();
+		console.log("e.target.value:", e.target.value);
+		setMessage(e.target.value);
+	};
+
+	const onFormSubmit = (message) => {
+
+		let userText = message.replace(/^\s+|\s+$/g, '');
+		// console.log("userText:", userText, userText.length)
+
+		if (userText.length === 0) {
+			console.log("Empty String")
+			setMessage("");
+			return
+		}
+		console.log("Message Sent:", message);
+		setMessage("");
+	};
+
+	const onKeyDown = (e) => {
+		if (e.code === "Enter" || e.code === "NumpadEnter") {
+			// console.log("Enter Detected");
+			onFormSubmit(e.target.value)
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("keydown", onKeyDown);
+		return () => {
+			window.removeEventListener("keydown", onKeyDown);
+		};
+	}, []);
+
 	return (
 		<div>
-			<form>
-				<label className="messageBox">
-					<input type="string" />
-					<button>Send</button>
-				</label>
-			</form>
+			<div className="messageBox">
+				<input
+					type="text"
+					value={message}
+					onChange={(e) => messageInput(e)}
+				/>
+				<button type="submit" onClick={() => onFormSubmit(message)}>
+					Send
+				</button>
+			</div>
 		</div>
 	);
 }
