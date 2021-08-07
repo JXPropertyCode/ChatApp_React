7fa3d8c750e486cd01345c816d00213a414c7813
JXPropertyCode <yellowdice92@hotmail.com>
2021-08-07 13:12:34 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./components/messageBox.css";
+
+var W3CWebSocket = require("websocket").w3cwebsocket;
+
+var client = new W3CWebSocket("ws://localhost:8000/", "echo-protocol");
+
+client.onerror = function () {
+	console.log("Connection Error");
+};
+
+// sending random numbers to Express's websocket, then Express would output them
+client.onopen = function () {
+	console.log("WebSocket Client Connected");
+	function sendNumber() {
+		if (client.readyState === client.OPEN) {
+			var number = Math.round(Math.random() * 0xffffff);
+			client.send(number.toString());
+			setTimeout(sendNumber, 1000);
+		}
+	}
+	sendNumber();
+};
+
+client.onclose = function () {
+	console.log("echo-protocol Client Closed");
+};
+
+client.onmessage = function (e) {
+	if (typeof e.data === "string") {
+		console.log("Received: '" + e.data + "'");
+	}
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<form>
+				<label className="messageBox">
+					<input type="string" />
+					<button>Send</button>
+				</label>
+			</form>
+		</div>
+	);
 }
 
 export default App;
