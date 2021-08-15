9216d9ce4b302d2a95395f9ea3716a58a1266deb
JXPropertyCode <yellowdice92@hotmail.com>
2021-08-15 12:43:02 -0700

    React WebSocket connected to Express WebSocket

--- src/App.js
+++ src/App.js
@@ -1,117 +1,27 @@
-import { useState } from "react";
-import "./components/messageBox.css";
-
-var W3CWebSocket = require("websocket").w3cwebsocket;
-
-var client = new W3CWebSocket("ws://localhost:8000/", "echo-protocol");
-
-client.onerror = function () {
-	console.log("Connection Error");
-};
-
-// sending random numbers to Express's websocket, then Express would output them
-client.onopen = function () {
-	console.log("WebSocket Client Connected");
-	// this is a setTimeout that constantly sends data to remind connection
-	// function sendNumber() {
-	// 	if (client.readyState === client.OPEN) {
-	// 		// var number = Math.round(Math.random() * 0xffffff);
-	// 		let date = new Date()
-	// 		let entireMessage = {
-	// 			"dateSent": date.toString(),
-	// 			"clientMessage": "Still Connected"
-	// 		}
-	// 		console.log("interval entireMessage:", entireMessage)
-	// 		client.send(JSON.stringify(entireMessage));
-	// 		// client.send({clientMessage: entireMessage});
-	// 		// client.send(new Date() + ": Still Connected");
-	// 		setTimeout(sendNumber, 10000);
-	// 	}
-	// }
-	// sendNumber();
-};
-
-client.onclose = function () {
-	console.log("echo-protocol Client Closed");
-};
-
-
-client.onmessage = function (e) {
-	// if (typeof e.data === "string") {
-	// 	console.log("Received: '" + e.data + "'");
-	// }
-
-	// console.log("e.data:", e.data)
-
-
-	// the e.data comes in a object format but in a string, so need to convert it to JSON
-	let dataReceived = JSON.parse(e.data)
-	console.log("dataReceived from Server:", dataReceived)
-	console.log("Message Received from Server: '" + dataReceived.clientMessage + "'");
-};
+import useWebSocket from "react-use-websocket";
 
 function App() {
+	// This can also be an async getter function. See notes below on Async Urls.
+	const socketUrl = "ws://localhost:8000/";
+	// ws://localhost:8000/
 
-	const [sendMessage, setSendMessage] = useState("")
-
-	const [messages, setMessages] = useState([])
-
-	const onFormSubmit = (e, sendMessage) => {
-		// prevents refresh, if you have form and onSubmit
-		// stops the browsers default behaviour
-		e.preventDefault();
-
-		// prevent whitespace messages
-		let userText = sendMessage.replace(/^\s+$/g, "");
-		if (userText.length === 0) {
-			console.log("Empty String");
-			setSendMessage("");
-			return;
-		}
-
-		if (client.readyState === client.OPEN) {
-			console.log("Message Sent:", sendMessage);
-			let date = new Date();
-			let entireMessage = {
-				dateSent: date.toString(),
-				clientMessage: sendMessage.toString(),
-			};
-
-			console.log("entireMessage Successfully Sent to Server:", entireMessage);
-			client.send(JSON.stringify(entireMessage));
-		} else {
-			console.log("client.readyState isn't connected");
-			let date = new Date();
-			let entireMessage = {
-				dateSent: date.toString(),
-				clientMessage: sendMessage.toString(),
-			};
-			console.log("entireMessage NOT Sent to Server:", entireMessage);
-		}
-		setSendMessage("");
-	};
+	const {
+		sendMessage,
+		sendJsonMessage,
+		lastMessage,
+		lastJsonMessage,
+		readyState,
+		getWebSocket,
+	} = useWebSocket(socketUrl, {
+		// if successfully connected to the Express WebSocket
+		onOpen: () => console.log("Successfully Connected to Express WebSocket..."),
+		// Will attempt to reconnect on all close events, such as server shutting down
+		shouldReconnect: (closeEvent) => true,
+	});
 
 	return (
-		<div>
-			<div className="chatDisplay">
-				{/* onFormSubmit() mechanism enables you to click the input box and pressing enter would trigger it only */}
-				<div className="messageWindow">
-				</div>
-				<form
-					className="messageBox"
-					onSubmit={(e) => onFormSubmit(e, sendMessage)}
-				>
-					<input
-						className="inputBox"
-						type="text"
-						value={sendMessage}
-						onChange={(e) => setSendMessage(e.target.value)}
-					/>
-					<button type="submit">Send</button>
-				</form>
-			</div>
-		</div>
-	);
+		<div>Hello</div>
+	)
 }
 
 export default App;
---
+++ src/PrevApp.js
@@ -0,0 +1,117 @@
+import { useState } from "react";
+import "./components/messageBox.css";
+
+var W3CWebSocket = require("websocket").w3cwebsocket;
+
+var client = new W3CWebSocket("ws://localhost:8000/", "echo-protocol");
+
+client.onerror = function () {
+	console.log("Connection Error");
+};
+
+// sending random numbers to Express's websocket, then Express would output them
+client.onopen = function () {
+	console.log("WebSocket Client Connected");
+	// this is a setTimeout that constantly sends data to remind connection
+	// function sendNumber() {
+	// 	if (client.readyState === client.OPEN) {
+	// 		// var number = Math.round(Math.random() * 0xffffff);
+	// 		let date = new Date()
+	// 		let entireMessage = {
+	// 			"dateSent": date.toString(),
+	// 			"clientMessage": "Still Connected"
+	// 		}
+	// 		console.log("interval entireMessage:", entireMessage)
+	// 		client.send(JSON.stringify(entireMessage));
+	// 		// client.send({clientMessage: entireMessage});
+	// 		// client.send(new Date() + ": Still Connected");
+	// 		setTimeout(sendNumber, 10000);
+	// 	}
+	// }
+	// sendNumber();
+};
+
+client.onclose = function () {
+	console.log("echo-protocol Client Closed");
+};
+
+
+client.onmessage = function (e) {
+	// if (typeof e.data === "string") {
+	// 	console.log("Received: '" + e.data + "'");
+	// }
+
+	// console.log("e.data:", e.data)
+
+
+	// the e.data comes in a object format but in a string, so need to convert it to JSON
+	let dataReceived = JSON.parse(e.data)
+	console.log("dataReceived from Server:", dataReceived)
+	console.log("Message Received from Server: '" + dataReceived.clientMessage + "'");
+};
+
+function App() {
+
+	const [sendMessage, setSendMessage] = useState("")
+
+	const [messages, setMessages] = useState([])
+
+	const onFormSubmit = (e, sendMessage) => {
+		// prevents refresh, if you have form and onSubmit
+		// stops the browsers default behaviour
+		e.preventDefault();
+
+		// prevent whitespace messages
+		let userText = sendMessage.replace(/^\s+$/g, "");
+		if (userText.length === 0) {
+			console.log("Empty String");
+			setSendMessage("");
+			return;
+		}
+
+		if (client.readyState === client.OPEN) {
+			console.log("Message Sent:", sendMessage);
+			let date = new Date();
+			let entireMessage = {
+				dateSent: date.toString(),
+				clientMessage: sendMessage.toString(),
+			};
+
+			console.log("entireMessage Successfully Sent to Server:", entireMessage);
+			client.send(JSON.stringify(entireMessage));
+		} else {
+			console.log("client.readyState isn't connected");
+			let date = new Date();
+			let entireMessage = {
+				dateSent: date.toString(),
+				clientMessage: sendMessage.toString(),
+			};
+			console.log("entireMessage NOT Sent to Server:", entireMessage);
+		}
+		setSendMessage("");
+	};
+
+	return (
+		<div>
+			<div className="chatDisplay">
+				{/* onFormSubmit() mechanism enables you to click the input box and pressing enter would trigger it only */}
+				<div className="messageWindow">
+				</div>
+				<form
+					className="messageBox"
+					onSubmit={(e) => onFormSubmit(e, sendMessage)}
+				>
+					<input
+						className="inputBox"
+						type="text"
+						value={sendMessage}
+						onChange={(e) => setSendMessage(e.target.value)}
+					/>
+					<button type="submit">Send</button>
+				</form>
+			</div>
+		</div>
+	);
+}
+
+export default App;
